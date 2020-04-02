52b89204efa7e7c014ac1e451b3560c875725b83
AnneLantunen <anne.lantunen@sadeinnovations.com>
2020-04-02 11:28:15 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/annen-oma-app.png';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <img src={logo} className="app-logo" alt="logo" />
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className="table-paper">
+                        Tähän tulee taulukko
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className="statistics-paper">
+                        Tähän tulee kuvaaja
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
